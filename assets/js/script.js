48b03381d99d13a429eb9290173a2a2246a7f104
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-10-13 21:33:57 +0000

    Amendments to the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,16 +98,20 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
+  let modalButton = document.getElementById('myBtn');
+  modalButton.textContent = "Click x to close";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+  let modalButtonA = document.getElementById('myBtn');
+  modalButtonA.textContent = "Click here for instructions";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+//window.onclick = function(event) {
+  //if (event.target == modal) {
+    //modal.style.display = "none";
+  //}
+//}
