1580d98b02f9806bb0b26281c251651795ae1d33
John Stuart Phillips <0q5av15l@students.codeinstitute.net>
2024-10-19 16:02:41 +0000

    Amend Modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -234,31 +234,46 @@ function updateEndMessage(endOfGame) {
 addListenersToControlButtons()
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function () {
+// btn.onclick = function () {
+//     modal.style.display = "block";
+//     let modalButton = document.getElementById('myBtn');
+//     modalButton.textContent = "Click x to close";
+// }
+
+btn.addEventListener("click", openInstructionsModal)
+function openInstructionsModal() {
     modal.style.display = "block";
     let modalButton = document.getElementById('myBtn');
     modalButton.textContent = "Click x to close";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
+// span.onclick = function () {
+//     modal.style.display = "none";
+//     let modalButtonA = document.getElementById('myBtn');
+//     modalButtonA.textContent = "Click here for instructions";
+// }
+
+modal.addEventListener("click", closeInstructionsModal)
+span.addEventListener("click", closeInstructionsModal)
+function closeInstructionsModal() {
     modal.style.display = "none";
     let modalButtonA = document.getElementById('myBtn');
     modalButtonA.textContent = "Click here for instructions";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-//window.onclick = function(event) {
-//if (event.target == modal) {
-//modal.style.display = "none";
-//}
-//}
+//When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+// if (event.target == modal) {
+// modal.style.display = "none";
+// }
+// }
